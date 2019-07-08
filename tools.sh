82ef662c7bcf687ba2b4a3ec6ef4ce3682b13245
aldihack <52653754+aldihack@users.noreply.github.com>
2019-07-08 15:47:29 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,17 @@
+clear
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pip install request
+pkg install php
+pkg install cowsay
+pkg install gem
+gem install lolcat
+pkg install figlet
+pkg install java-sdk
+echo Hallo!,This Tools Is Beta
+echo #DontRecode
+echo If You Have All Tools!Dont Used :)
+clear
